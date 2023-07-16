968f5568641eb9c3a0c5100a99527665c8054fd0
Maria Durrani <maria_durrani@hotmail.com>
2023-07-16 03:09:44 +0200

    api genre

--- script.js
+++ script.js
@@ -3,6 +3,101 @@ const url = `https://api.themoviedb.org/3/movie/popular?api_key=6b2d670159350152
 const imgUrl = `https://image.tmdb.org/t/p/w500`;
 const movielistItem = document.getElementById(`movielist-item`);
 
+const genres = [
+        {
+            "id": 28,
+            "name": "Action"
+        },
+        {
+            "id": 12,
+            "name": "Adventure"
+        },
+        {
+            "id": 16,
+            "name": "Animation"
+        },
+        {
+            "id": 35,
+            "name": "Comedy"
+        },
+        {
+            "id": 80,
+            "name": "Crime"
+        },
+        {
+            "id": 99,
+            "name": "Documentary"
+        },
+        {
+            "id": 18,
+            "name": "Drama"
+        },
+        {
+            "id": 10751,
+            "name": "Family"
+        },
+        {
+            "id": 14,
+            "name": "Fantasy"
+        },
+        {
+            "id": 36,
+            "name": "History"
+        },
+        {
+            "id": 27,
+            "name": "Horror"
+        },
+        {
+            "id": 10402,
+            "name": "Music"
+        },
+        {
+            "id": 9648,
+            "name": "Mystery"
+        },
+        {
+            "id": 10749,
+            "name": "Romance"
+        },
+        {
+            "id": 878,
+            "name": "Science Fiction"
+        },
+        {
+            "id": 10770,
+            "name": "TV Movie"
+        },
+        {
+            "id": 53,
+            "name": "Thriller"
+        },
+        {
+            "id": 10752,
+            "name": "War"
+        },
+        {
+            "id": 37,
+            "name": "Western"
+        }
+    ]
+
+
+const tagsEl = document.getElementById(`tags`);
+
+setGenre();
+function setGenre() {
+    tagsEl.innerHTML = '';
+    genres.forEach(genre => {
+        const t = document.createElement('ul')
+        t.classList.add('tag')
+        t.id=genre.id;
+        t.innerText = genre.name;
+        tagsEl.appendChild(t);
+    })
+}
+
+
 getMovies(url);
 
 function getMovies(url) {
